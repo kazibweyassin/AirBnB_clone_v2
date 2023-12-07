45f1a15c7c065be900a9ee08c849ea800a92d93e
Abdul Rahman <abdulnsamba@gmail.com>
2023-12-08 00:41:26 +0300

    pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,7 +14,7 @@ def do_pack():
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("version") is False:
-            local("mkdir versions")
+            local("mkdir -p versions")
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
