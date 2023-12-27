429704c27eee69abefc5d82de7edb82e4339daad
Abdul Rahman <abdulnsamba@gmail.com>
2023-12-27 19:31:51 +0300

    modify

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,14 +4,17 @@
     Hello route
 """
 
-from . import app
+from flask import Flask
 
 
-@app.route('/')
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
 def hello():
     """ Hello world"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port='5000', host='0.0.0.0')
--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,7 +1,1 @@
-"""
-    init.py file to initialize
-"""
 
-from flask import Flask
-
-app = Flask(__name__)
