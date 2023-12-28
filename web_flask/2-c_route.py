6aaa047574583947dfe732296aabac919533d96b
Abdul Rahman <abdulnsamba@gmail.com>
2023-12-28 11:31:32 +0300

    c is cool

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+    Hello route
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """ Hello world"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello(text):
+    """ With a parameter"""
+    formatted = text.replace('_', ' ')
+    return f"C {formatted}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port='5000', host='0.0.0.0')
